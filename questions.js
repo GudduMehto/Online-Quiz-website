e763812e977f125f1b7861c0d486b86e8b5aec4a
GudduMehto <145039093+GudduMehto@users.noreply.github.com>
2023-10-05 18:04:36 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question:"What does HTML stand for?",
+        answer:"C. Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language", 
+            "C. Hyper Text Markup Language",
+            "D. Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question:"What does CSS stand for?",
+        answer:"A. Cascading Style Sheet",
+        options:[
+            "A. Cascading Style Sheet", 
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Codehal Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question:"What does PHP stand for?",
+        answer:"A. Hypertext Preprocessor",
+        options:[
+            "A. Hypertext Preprocessor",
+            "B. Hypertext Programming",
+            "C. Hometext Preprogramming", 
+            "D. Programming Hypertext Preprocessor"
+        
+        ]
+    },
+    {
+        numb: 4,
+        question:"What does SQL stand for?",
+        answer:"D. Structured Query Language",
+        options:[
+            "A. Hypertext Preprocessor",
+            "B. Strength Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question:"The latest HTML standard is?",
+        answer:"B. HTML 5.0",
+        options:[
+            "A. HTML 4.0",
+            "B. HTML 5.0",
+            "C. XML",
+            "D. SGML",
+        ]
+    },
+];
